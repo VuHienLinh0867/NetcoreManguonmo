42b10ea9d9f17b6564ef8f1d2f0067599a6a72a4
Vu Hien Linh <hienlinh0211@gmail.com>
2021-11-18 14:04:22 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetcoreManguonmo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
